480c29398631d5b7bbf3b7840f35f18d30dc95c8
OlgaLukashevich <kosochek@gmail.com>
2024-08-07 09:54:53 +0300

    Initial commit

--- app/src/main/java/com/example/tickets_2/MainActivity.kt
+++ app/src/main/java/com/example/tickets_2/MainActivity.kt
@@ -62,6 +62,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var datePicker: DatePicker
 
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
